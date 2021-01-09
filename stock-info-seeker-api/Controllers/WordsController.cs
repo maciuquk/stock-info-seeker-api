6a0be0ae57d8f4bec741fa58e549e5f20ff08988
maciuquk <maciejromik@gmai.com>
2021-01-09 18:30:25 +0100

    add words controller

---
+++ stock-info-seeker-api/Controllers/WordsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using stock_info_seeker_api.Model;
+
+namespace stock_info_seeker_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WordsController : ControllerBase
+    {
+        private readonly ProjectContext _context;
+
+        public WordsController(ProjectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Words
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SeekFor>>> GetseekFor()
+        {
+            return await _context.seekFor.ToListAsync();
+        }
+
+        // GET: api/Words/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SeekFor>> GetSeekFor(int id)
+        {
+            var seekFor = await _context.seekFor.FindAsync(id);
+
+            if (seekFor == null)
+            {
+                return NotFound();
+            }
+
+            return seekFor;
+        }
+
+        // PUT: api/Words/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutSeekFor(int id, SeekFor seekFor)
+        {
+            if (id != seekFor.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(seekFor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SeekForExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Words
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<SeekFor>> PostSeekFor(SeekFor seekFor)
+        {
+            _context.seekFor.Add(seekFor);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSeekFor", new { id = seekFor.Id }, seekFor);
+        }
+
+        // DELETE: api/Words/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSeekFor(int id)
+        {
+            var seekFor = await _context.seekFor.FindAsync(id);
+            if (seekFor == null)
+            {
+                return NotFound();
+            }
+
+            _context.seekFor.Remove(seekFor);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool SeekForExists(int id)
+        {
+            return _context.seekFor.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ stock-info-seeker-api/Migrations/20210109165334_a1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace stock_info_seeker_api.Migrations
+{
+    public partial class a1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "occurence",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Event = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_occurence", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "seekFor",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Word = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_seekFor", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "occurence");
+
+            migrationBuilder.DropTable(
+                name: "seekFor");
+        }
+    }
+}
